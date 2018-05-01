919769d5c677e622e6e4cf8b85f74cb584210cb4
Paul Schaefer <yeppaul2@gmail.com>
2018-05-01 16:49:20 -0400

    Tweaked header responsiveness, added menu

--- header.php
+++ header.php
@@ -10,20 +10,20 @@
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
-		<header class="mt5 ph3">
-			<h1 class="tc fw3 ttu tracked lh-solid">
+		<header class="mt5 ph3 tc">
+			<h1 class="f2-ns f3 fw3 ttu tracked lh-solid">
 				<a href="<?php echo get_site_url(); ?>">
 					<img src="<?php echo get_template_directory_uri(); ?>/includes/logo.svg" alt="<?php bloginfo('name'); echo ' '; bloginfo('description'); ?>" class="center db w-100 mw6 mb3" />
 				</a>
 				<?php bloginfo('name'); ?>
 				<br />
-				<span class="gray f3">
+				<span class="gray f3-ns f4">
 					<?php bloginfo('description'); ?>
 				</span>
 			</h1>
-			<?php /* wp_nav_menu(array(
+			<?php wp_nav_menu(array(
 				'theme_location' => 'first_menu',
-				'menu_class'     => 'col',
+				'menu_class'     => 'list pl0 ttu',
 				'container'      => 'nav',
-			)); */ ?>
+			)); ?>
 		</header>
