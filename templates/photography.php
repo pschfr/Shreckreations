09d81617c630b0fca8d2a12abf1274323b4d717f
Paul Schaefer <yeppaul2@gmail.com>
2018-05-01 14:12:28 -0400

    Added empty page templates

---
+++ templates/photography.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Template Name: Photography Page
+*/
+?>
+<?php get_header(); ?>
+<section id="content" role="main" class="bg-black white pv3">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <section class="entry-content">
+                <?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
+                <?php the_content(); ?>
+                <div class="entry-links"><?php wp_link_pages(); ?></div>
+            </section>
+        </article>
+        <?php if ( ! post_password_required() ) comments_template( '', true ); ?>
+        <?php endwhile; endif; ?>
+</section>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
